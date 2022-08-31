9ef00c5069d65212465d7e4c20b79cb455449674
raininsnovv <raininsnovv@gmail.com>
2022-08-31 20:00:42 +0300

    Created todo-list

---
+++ main.js
@@ -0,0 +1,53 @@
+const todoList = {
+  items: [
+    {
+      text: "Завершить текущий челлендж",
+      completed: false,
+    },
+    {
+      text: "Отдохнуть во время перерыва",
+      completed: false,
+    },
+    {
+      text: "Помочь напарнику понять код",
+      completed: false,
+    },
+    {
+      text: "Выиграть в мафию",
+      completed: true,
+    },
+  ],
+
+  printAll: function () {
+    for (let i = 0; i < this.items.length; i++) {
+      this.print(i);
+    }
+  },
+
+  add: function (text, completed) {
+    this.items.unshift({ text: text, completed: false });
+  },
+
+  remove: function (index) {
+    this.items.splice(index, 1);
+  },
+
+  print: function (index) {
+    if (this.items[index].completed === false) {
+      console.log(`[ ] ${this.items[index].text}`);
+    } else {
+      console.log(`[x] ${this.items[index].text}`);
+    }
+  },
+
+  complete: function (index) {
+    this.items[index].completed = true;
+  },
+};
+
+todoList.add("Подпрыгнуть два раза");
+todoList.remove(0);
+todoList.printAll();
+todoList.complete(0);
+todoList.print(0);
+todoList.print(3);
